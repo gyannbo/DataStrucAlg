72525d30560aaef384a5f899218dc7485bcaf755
gbonis <gyann.bonis@gmail.com>
2025-07-26 01:18:18 +0200

    cleaned up two sum

--- TwoSum.c
+++ TwoSum.c
@@ -1,39 +1,46 @@
+// given an array, return the indices of two number that add to a given sum
+// example :
+// array[] = {1, 2, 3, 4}
+// target = 5
+// answer[2] ={0, 3}
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-   int	i = 0;
-   int	y = 1;
+	int	indexFirstNum = 0;
+	int	indexSecNum = 1;
 
-	int *ans = malloc(sizeof(int) * 2);
-	if (!ans)
+	int *answer = malloc(sizeof(int) * 2);
+	if (!answer)
 		return NULL;
-	while(i < numsSize)
+	while(indexFirstNum < numsSize)
 	{
-		while(y < numsSize) 
+		while(indexSecNum < numsSize) 
 		{
-			if (nums[i] + nums[y] == target)
+			if (nums[indexFirstNum] + nums[indexSecNum] == target)
 			{
-				ans[0] = i;
-				ans[1] = y;
+				answer[0] = indexFirstNum;
+				answer[1] = indexSecNum;
 				*returnSize = 2;
-				return ans;
+				return answer;
 			}
-			y++;
+			indexSecNum++;
 		}
-		y = (i + 2);
-		i++;
+		indexSecNum = (indexFirstNum + 2);
+		indexFirstNum++;
 	}
 	return (NULL);
 }
 
 int	main()
 {
-	int nums[4] = { 1, 2, 3, 4};
-	int target = 3;
-	int	*ans;
+	int nums[4] = {1, 2, 3, 4};
+	int target = 5;
+	int	*ans = NULL;
+	int returnSize = 0;
 
-	ans = twoSum(nums, 4, target, NULL);
+	ans = twoSum(nums, 4, target, &returnSize);
 	printf("%d \n%d\n", ans[0], ans[1]);
 	free(ans);
 	return (0);

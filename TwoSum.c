eff91bab44cfcc14c115ccc5bea518def5deed57
gbonis <gbonis@debian.gbonis>
2025-07-30 09:59:45 +0200

    added leetcode Hash solution to twosum

--- TwoSum.c
+++ TwoSum.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <uthash.h>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
    int	i = 0;
@@ -40,4 +41,39 @@ int	main()
 }	
 
 
-// check the hash tables to do the solution that is more performant, and understand why		
+
+// Leetcode hashmap solution using uthash library
+//
+//int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+//    struct hashTable {
+//        int key;
+//        int value;
+//        UT_hash_handle hh;
+//    } *hashTable = NULL, *item, *tmpItem;
+//
+//    for (int i = 0; i < numsSize; i++) {
+//        HASH_FIND_INT(hashTable, &nums[i], item);
+//        if (item) {
+//            int* result = malloc(sizeof(int) * 2);
+//            result[0] = item->value;
+//            result[1] = i;
+//            *returnSize = 2;
+//            HASH_ITER(hh, hashTable, item, tmpItem) {
+//                HASH_DEL(hashTable, item);
+//                free(item);
+//            }
+//            return result;
+//        }
+//        item = malloc(sizeof(struct hashTable));
+//        item->key = target - nums[i];
+//        item->value = i;
+//        HASH_ADD_INT(hashTable, key, item);
+//    }
+//    HASH_ITER(hh, hashTable, item, tmpItem) {
+//        HASH_DEL(hashTable, item);
+//        free(item);
+//    }
+//    *returnSize = 0;
+//    // If no valid pair is found, return an empty array
+//    return malloc(sizeof(int) * 0);
+//}

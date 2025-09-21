dbbb6e2f9f55c7420e9a680244030d10029c8eed
gbonis <gyann.bonis@gmail.com>
2025-09-21 20:38:54 +0200

    hashmap

--- hash/hashmap.c
+++ hash/hashmap.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE
+#define _GNU_SOURCE	     // needed for reentrant version of this library
 #include <search.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,45 +6,33 @@
 
 #define NUMBOFKEYS 111
 
-//ENTRY *HashFunc(char *s)
-//{
-//	ENTRY *data = malloc(sizeof(ENTRY));
-//	int i = 0;
-//	unsigned int result = 0;
-//
-//	while(s[i])
-//	{
-//		result += s[i];
-//		i++;
-//	}
-//
-//	// on va utiliser sprintf pour voir, plutot que itoa, histoire d'utiliser des nouvelles fonctions
-//	data->key = itoa(result % NUMBOFKEYS);		
-//	data->data = s;
-//	return(data);
-//}
-//
 int main()
 {
 	char *tab[] = {"1234", "2222" ,"44" ,"3" ,"554" ,"66152", "4432" ,"5"};
 	unsigned int size = sizeof(tab) / sizeof(tab[0]);	
+
+// char buffer because need to work with strings as data, not int. Using sprintf as an itoa //
 	char stringmem[100];
 	ENTRY * retval = NULL;
 
+//	initialising the struct, otherwise segfault/valgrind error, still dont fully understand why this line works, because //
+//	i wasnt supposed to access this struct members as said in the lib doc //
 	struct hsearch_data htab = { 0 };
 	hcreate_r(NUMBOFKEYS, &htab);
 	int i = 0;
 
 	while ( i < size)
 	{
+
+		// needed to convert to int the string data in order to modulo number of keys //
 		sprintf(stringmem, "%d", atoi(tab[i]) % NUMBOFKEYS); 
-		ENTRY ex ;
-		ex.key = stringmem;
-		ex.data = tab[i];
-		//hsearch_r(ex, ENTER, &retval, &htab);
+
+		//  using a compound literal to loop on ENTER data in hashtable //
 		hsearch_r((ENTRY){ stringmem, tab[i]}, ENTER, &retval, &htab);
-		hsearch_r(ex, FIND, &retval, &htab);
-		//printf("%s\n", retval->data);
+
+		// SHOULD DO ANOTHER WHILE HERE, DOESNT MAKE SENSE THIS WAY
+		hsearch_r((ENTRY){ stringmem, tab[i]}, FIND, &retval, &htab);
+		printf("%s\n", retval->data);
 		printf("%s\n", retval->key); 
 		i++;
 	}
@@ -58,10 +46,8 @@ int main()
 // 		-avoir la data en strings
 // 		-comme je pensais je pouvais avoir la struct keys/data en compound literal`
 
-
-//use fundamentals of data struc in c book
-//non, utiliser plutot le intro to algo
-
+//utiliser intro to algo pour comprendre les hashtables
+//
 // j'ai un peu compris le principe de l'utilisation de la hash table sur two sum:
 // on met dans la hashtable les nombres qu'on nous donne, et ensuite c'est sur le lookup qu'on va gagner beaucoup
 // de temps. On calcule d'abord vis à vis du chiffre qu'il faudrait pour atteindre la bonne somme selon le numéro
@@ -77,6 +63,7 @@ int main()
 
 // do division method, multiplication method, and universal hashing avaec des keys statiques
 
+
 // je sais pas si je vais réussir tout de suite à faire le universal hashing car faut vraiment bien comprendre les
 // math pour pas se planter sur la deuxieme table de hachage, ça va me prendre du temps.
 //
@@ -84,14 +71,17 @@ int main()
 // Aussi noter la technique pour avoir ce qu'il y a après la virugule: mod 1, et comprendre pourquoi il y a les rounding
 // error sur les float
 
+
 // On va plutot essayer de faire avec la libc déjà, donc avec hcreate
 // regarder pour le crc32 pour faire une hash fonction maison
+
 
 // https://stackoverflow.com/questions/190229/where-is-the-itoa-function-in-linux
 //  http://www.burtleburtle.net/bob/hash/doobs.html
 // https://www.reddit.com/r/programminghorror/comments/uitjxl/hsearch_why_is_this_crap_in_libc/
 // pour l'instant on va juste faire au plus simple, on additionne tous les chars de la string. On verra après pour faire des meilleures
 // hash func.
+
 
 //----------
 //4-9-25
